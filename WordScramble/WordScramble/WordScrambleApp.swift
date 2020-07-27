3fffe656f7c568401f435a11b720956c4a2fc6d4
brandonlaw <brandonxlaw@gmail.com>
2020-07-26 21:47:58 -0400

    Add project files

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Brandon Law on 2020-07-26.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
